578540f07aad9be025fc327f0f4cf063f4cb4920
huzaifa <164413458+m5huzefa@users.noreply.github.com>
2025-02-06 06:24:30 +0500

    3nd commit

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
 import { SanityLive } from "@/sanity/lib/live";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider dynamic>
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <main>
             <Header/>
         {children}
